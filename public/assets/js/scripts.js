cdc7540715fd5f721ff3e6c41ce5bb852b4d19e5
Monica Chu <chuxmo@edc.ca>
2018-02-20 10:42:07 -0500

    added fa

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,16 +1,4 @@
-var feed = new Instafeed({
-    get: 'user',
-    userId: '22975752',
-    accessToken:'22975752.2a7da3a.1480ebd7ccad458dad109aa95357c24f',
-    limit: '10',
-    sortBy: 'most-recent',
-    // target: 'instafeed',
-    //tagName: 'awesome',
-    //resolution: 'standard_resolution',
-    clientId: '2a7da3a55dab4e5da71adfa0448293c7',
-    template: '<img src="{{image}}" alt="{{caption}}" />'
-});
-feed.run();
+
 $('.open-overlay').click(function() {
     var overlay_navigation = $('.overlay-navigation'),
       nav_item_1 = $('nav li:nth-of-type(1)'),
